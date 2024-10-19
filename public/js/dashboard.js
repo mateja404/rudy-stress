7bb80627c495ec1db7d40b555419f18f47d8d762
Marko Jovanovic <markojovanovicc23@outlook.com>
2024-10-19 20:39:58 +0200

    Dodat chart.js

---
+++ public/js/dashboard.js
@@ -0,0 +1,52 @@
+let chartInstance;
+
+function fetchAndUpdateChart() {
+    // Staticki podaci
+    const labels = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'];
+    const counts = [12, 19, 3, 5, 2]; // Primer brojeva
+
+    const backgroundColors = [
+        'rgb(255, 99, 132)',
+        'rgb(54, 162, 235)',
+        'rgb(255, 206, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(153, 102, 255)'
+    ];
+
+    const chartData = {
+        labels: labels,
+        datasets: [{
+            label: 'Attacks',
+            data: counts,
+            backgroundColor: backgroundColors,
+            hoverOffset: 4
+        }]
+    };
+
+    if (chartInstance) {
+        chartInstance.data = chartData;
+        chartInstance.update();
+    } else {
+        const config = {
+            type: 'doughnut',
+            data: chartData,
+            options: {
+                plugins: {
+                    legend: {
+                        display: true,
+                    }
+                }
+            }
+        };
+
+        const ctx = document.getElementById("noviChart").getContext("2d");
+        chartInstance = new Chart(ctx, config);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchAndUpdateChart();
+    // Ukloni setInterval ako ne želiš osvežavanje
+    // setInterval(fetchAndUpdateChart, 5 * 60 * 1000); // Refresh every 5 minutes
+    console.log("lalala")
+});
