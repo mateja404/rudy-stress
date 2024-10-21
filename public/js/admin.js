1bcbda038ae1b4fc1788af4aa955a8943e1eec75
Marko Jovanovic <markojovanovicc23@outlook.com>
2024-10-21 22:30:13 +0200

    Admin(WIP)

--- public/js/admin.js
+++ public/js/admin.js
@@ -6,3 +6,36 @@ const sidebar = document.querySelector(".sidebar");
 menuButton.addEventListener("click", function() {
     sidebar.classList.toggle("active-sidebar");
 });
+
+// tabs
+
+// tabs
+
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+const tabButtons = document.querySelectorAll('.tabButton');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('actives');
+        });
+
+        const target = document.querySelector(tab.dataset.tabTarget);
+        if (target) {
+            target.classList.add('actives');
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tabButtons = document.querySelectorAll('.tabButton');
+
+    tabButtons.forEach(tabButton => {
+        tabButton.addEventListener('click', () => {
+            tabButtons.forEach(btn => btn.classList.remove('selected'));
+
+            tabButton.classList.add('selected');
+        });
+    });
+});
